ecb8b70b4d884f227df5d31a3db010aa84769227
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-09-22 21:37:54 -0300

    Add eye-outline and eye-off-outline icons to ICONS and update index.ts

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,6 @@
+export { default as LoginScreen } from "./LoginScreen.tsx";
+// export { default as RegisterScreen } from "./RegisterScreen.tsx";
 export { default as HomeScreen } from "./HomeScreen.tsx";
 export { default as CounterScreen } from "./CounterScreen.tsx";
 export { default as AboutScreen } from "./AboutScreen.tsx";
 export { default as PokeScreen } from "./PokeScreen.tsx";
-export { default as LoginScreen } from "./LoginScreen.tsx";
